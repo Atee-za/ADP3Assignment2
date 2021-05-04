9ff95f9dff1112e53903c96f5ddfc7aaae601bd1
Athenkosi <athenkosizono@gmail.com>
2021-05-04 19:16:13 +0200

    Added .gitignore and the test file

---
+++ src/test/java/za/ac/cput/collections/ContactsTest.java
@@ -0,0 +1,12 @@
+package za.ac.cput.collections;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
